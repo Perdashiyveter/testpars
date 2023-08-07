d9f6d0d30bbaa7678a7175ea44abedb91f2ee378
Perdashiyveter <tima6098@mail.ru>
2023-08-08 01:00:08 +0400

    fisrt commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://yandex.ru/pogoda/saratov?lat=51.533561&lon=46.034265'
+response = requests.get(url)
+bs = BeautifulSoup(response.text,"lxml")
+print(bs)
+temp = bs.find('span','temp__value temp__value_with-unit')
+print(temp.text)
