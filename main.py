080d92390019bfba1565240022c342983a9b785d
Perdashiyveter <tima6098@mail.ru>
2023-08-08 01:15:48 +0400

    commit 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 
 url = 'https://yandex.ru/pogoda/saratov?lat=51.533561&lon=46.034265'
 response = requests.get(url)
+print(response)
 bs = BeautifulSoup(response.text,"lxml")
 print(bs)
 temp = bs.find('span','temp__value temp__value_with-unit')
